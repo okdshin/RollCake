1498e255351ecdeeb67d8a13f719f7ee16806cb0
s.okada <kokuzen@gmail.com>
2012-11-03 17:57:05 +0900

    fixed.

--- pw.js
+++ pw.js
@@ -77,7 +77,7 @@
 				$('#result').prepend(
 					'<div class = "msg box">'+cmd.username+' login.'+'</div>');
 				$('#online_user_list').prepend(
-					'<span class="user_id_'+cmd.username+'" id='+cmd.username+'>'+cmd.username+' </span>');
+					'<span class="user_name_'+cmd.username+'" id='+cmd.userId+'>'+cmd.username+' </span>');
 			}	
 			else if (cmd.command === 'removeUser'){
 				$('#result').prepend(
